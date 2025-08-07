c37120e5f563092e9b6854d55f4d9fe5cf3e2742
Abdallah Abdelkhalek <abdallah.abdelkhalek98@gmail.com>
2025-08-08 00:28:35 +0300

    Responsive Navbar with search, navigations and theme switch

---
+++ config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: `Abdallah's weather-app`,
+  description:
+    'Weather app with geolocation',
+  navItems: [
+    {
+      label: 'Home',
+      href: '/',
+    },
+  ],
+  navMenuItems: [
+    {
+      label: 'Profile',
+      href: '/',
+    },
+    {
+      label: 'settings',
+      href: '/',
+    },
+  ],
+  links: {
+    github: 'https://github.com/Abdallah-Abdelkhalek',
+  },
+};
